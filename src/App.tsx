aed9081437fa5330b957c2a272f9a235b16824f3
supercode0228 <bee0228angel@outlook.com>
2021-05-26 15:52:30 -0400

    add store to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { Home } from './pages';
+import store from './store';
+
+import './App.css';
 
 const App: React.FunctionComponent = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   )
 };
 
