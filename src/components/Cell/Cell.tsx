2c556bb47454976942f5ddc7d1b3c0413fc3ac8b
supercode0228 <bee0228angel@outlook.com>
2021-05-26 14:34:34 -0400

    define the board cell component

---
+++ src/components/Cell/Cell.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { Cell, CellStatus } from '../../types';
+
+import './Cell.css';
+
+type CellProps = Cell & {
+  onClick(): void
+  onCtrlClick(): void
+};
+
+const Field: React.FunctionComponent<CellProps> = props => {
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (e.ctrlKey) {
+      props.onCtrlClick();
+    } else {
+      props.onClick();
+    }
+  }
+  const {
+    status,
+    revealedCount,
+  } = props;
+
+  let icon = <i>.</i>;
+  let className = 'cell';
+  if (status === CellStatus.Exploded) {
+    icon = <i className="fa fa-bomb" />;
+    className += ' cell--exploded';
+  } else if (status === CellStatus.Revealed) {
+    icon = <i>{revealedCount > 0 ? revealedCount.toString() : '.'}</i>;
+    className += ` cell--revealed cell--revealed-${revealedCount}`;
+  } else if (status === CellStatus.Flagged) {
+    icon = <i className="fa fa-flag" />;
+    className += ' cell--flagged';
+  }
+
+  return (
+    <button
+      className={className}
+      onClick={onClick}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default Field;
